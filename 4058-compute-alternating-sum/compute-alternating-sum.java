a5a7a7a67def4c691283cfb49c2f4360d2d4adfc
Shivraj Jat <JATSHIVRAJ130@GMAIL.COM>
2026-09-11 15:55:13 +0530

    Time: 1 ms (96.17%) | Memory: 46.3 MB (82.83%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int evenSum = 0;
+        int oddSum = 0;
+        for(int i=0; i<nums.length; i++){
+            if(i%2==0){
+                evenSum = evenSum + nums[i];
+            }
+            else{
+                oddSum = oddSum + nums[i];
+            }
+        }
+        return evenSum-oddSum;
+    }
+}
