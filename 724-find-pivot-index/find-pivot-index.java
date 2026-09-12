0cdaddd533a33ea52eb398315ca726394cf3f1fd
Shivraj Jat <JATSHIVRAJ130@GMAIL.COM>
2026-09-12 17:59:20 +0530

    Time: 1 ms (96.47%) | Memory: 47.5 MB (22.50%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,26 +1,48 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
+
         
-
-        prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
+        // int[] prefix = new int[n];
+        // int[] suffix = new int[n];
+        // prefix[0] = nums[0];
+        // suffix[n-1] = nums[n-1];
  
-        for(int i=1; i<n; i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
+        // for(int i=1; i<n; i++){
+        //     prefix[i] = prefix[i-1] + nums[i];
+        // }
 
-        for(int i=n-2; i>=0; i--){
-            suffix[i] = suffix[i+1] + nums[i];
-        }
+        // for(int i=n-2; i>=0; i--){
+        //     suffix[i] = suffix[i+1] + nums[i];
+        // }
 
-        for(int i=0; i<n; i++){
-            if(prefix[i] == suffix[i]){
-                return i;
-            }
-        }
+        // for(int i=0; i<n; i++){
+        //     if(prefix[i] == suffix[i]){
+        //         return i;
+        //     }
+        // }
+
+ /*
+ Optimised code:- we optimised space here 
+ */
+
+ int prefix = 0;
+ int suffix = 0;
+ int total = 0;
+
+ for(int i=0; i<n; i++){
+    total = total + nums[i];
+ }
+
+ for(int i=0; i<n; i++){
+    suffix = total - prefix;
+    prefix += nums[i];
+
+    if(prefix == suffix){
+        return i;
+    }
+ }
+
 return -1;
     }
 }
