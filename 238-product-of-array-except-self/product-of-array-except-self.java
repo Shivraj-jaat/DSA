c858a997912be0f71c2cbb33abc8a2be4263d71f
Shivraj Jat <JATSHIVRAJ130@GMAIL.COM>
2026-09-12 17:29:01 +0530

    Time: 2 ms (93.13%) | Memory: 65 MB (82.62%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -8,8 +8,8 @@ class Solution {
         prefix[0] = 1;
         suffix[n] = 1;
 
-        for(int i=1; i<n+1; i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+        for(int i=0; i<n; i++){
+            prefix[i+1] = prefix[i] * nums[i];
         }
         for(int i=n-1; i>=0; i--){
             suffix[i] = suffix[i+1] * nums[i];
